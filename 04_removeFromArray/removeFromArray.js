b329fc3c43374d0ece7eb62f22ce84f0e41118a9
Remission94 <vmajkic94@gmail.com>
2024-06-06 20:11:33 +0200

    First 5 exercises done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(arrayInput, ...toRemove) {
+    const toRemoveArray = Array.from(toRemove);
+
+    for (i=0; i < toRemoveArray.length; i++) {
+        for (n=0; n < arrayInput.length; n++) {
+            for (e=0; e < arrayInput.length; e++) {
+                if (arrayInput[n] === toRemoveArray[i]) {
+                arrayInput.splice(n,1)
+            }
+        }
+        }
+    }
+
+    return arrayInput
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
-
-};
+const sumAll = function(x,y) {
+    let sum = 0
+    
+    if (x < 0 || y < 0) {
+        return "ERROR"
+    } else if (typeof x === "string" || typeof y === "string" || typeof x === "object" || typeof y === "object") {
+        return "ERROR"
+    }
+    else if (x < y) {
+        for (let i=x; i <= y; i++) {
+            sum += i
+        }
+        return sum
+    } else if (x > y) {
+        for (let i=y; i <= x; i++) {
+            sum += i
+        }
+        return sum   
+    }
+}
 
 // Do not edit below this line
 module.exports = sumAll;
