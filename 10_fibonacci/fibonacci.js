7c981a393ee0c9badecf69b6e8a7f5a9bb78475d
Remission94 <vmajkic94@gmail.com>
2024-06-16 09:49:46 +0200

    Fibonacci finished

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let arrayStart = [0,1];
+    
+    if (num < 0) return "OOPS"
+    if (num === 0) return 0
 
+    for (i = 2; i <= num; i++) {
+        arrayStart[i] = arrayStart[i-1] + arrayStart[i-2]
+    }
+    return arrayStart.at(-1)
 };
 
 // Do not edit below this line
