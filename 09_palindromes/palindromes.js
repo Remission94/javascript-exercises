aa53a76f8ee884be6b7694a4a4f2e9f743313011
Remission94 <vmajkic94@gmail.com>
2024-06-16 08:38:54 +0200

    Exercise 8 and 9 finished

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const ALPHA = "qwertzuiopasdfghjklyxcvbnm1234567890"
 
+const palindromes = function (word) {
+    let clearString = word
+    .toLowerCase()
+    .split("")
+    .filter((letter) => ALPHA.includes(letter))
+    .join("")
+
+    let reversedString = clearString.split("").reverse().join("")
+    
+    return reversedString === clearString
 };
 
 // Do not edit below this line
